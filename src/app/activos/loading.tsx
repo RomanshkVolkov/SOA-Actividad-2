7960129f08910dc791b535d74f21443100b642b9
azulcolor <lealpastelin@gmail.com>
2023-05-31 17:56:33 -0500

    Test commit

--- src/app/activos/loading.tsx
+++ src/app/activos/loading.tsx
@@ -1,7 +1,9 @@
 export default function Loading() {
-   return (
+  return (
+    <>
       <div className="flex items-center justify-center">
-         <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
       </div>
-   );
+    </>
+  );
 }
