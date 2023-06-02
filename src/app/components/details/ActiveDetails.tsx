7a7b6e5f4c02bbd7f552351b9a17791abb3fe92b
Jose Guzman <joseguzmandev@gmail.com>
2023-06-02 17:37:40 -0500

    UPT

--- src/app/components/details/ActiveDetails.tsx
+++ src/app/components/details/ActiveDetails.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 'use client';
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import ItemInput from '../Inputs/ItemInput';
 import ItemOptions from '../Inputs/ItemOptions';
